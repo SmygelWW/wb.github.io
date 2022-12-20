2bd50738c8c7a0672c96629adb2f73bda92fdd3b
SmygelWW <a214768206@126.com>
2022-12-20 17:17:08 +0800

    1

--- eeyes/script.js
+++ eeyes/script.js
@@ -1,3 +1,9 @@
+document.onreadystatechange = subSomething;//当页面加载状态改变的时候执行这个方法. 
+function subSomething() 
+{ 
+if(document.readyState == "complete"){ 
+
+
 const canvas = document.getElementsByTagName("canvas")[0];
 const context = canvas.getContext("2d");
 canvas.width=window.innerWidth;
@@ -243,78 +249,4 @@ originY=canvas.height/2;
     // document.onclick=index()
   })();//背景
 
-
-
-
-
-
-
-
-
-// function index(){
-//     var str=h1.innerHTML
-//     if(str=="TAP TO<br>CONTINU<span class=\"last\">E</span>"){}
-//     else{showindex()}
-// }
-
-// function showindex(){
-// //定义变量
-// var ScaleX=0.05;
-// var ScaleY=0.05;
-// var initialPointsNum=50;
-// var points;
-
-// clear();
-
-
-// points=randomPoints(initialPointsNum);
-
-// drawPoints(points);
-
-// setInterval(function fly(){
-//     clear();
-//     drawPoints(points);
-//     points=points.concat(randomPoints(initialPointsNum));
-//     movePoints(points);
-// },10);
-
-
-// //函数
-// //清空
-// function clear(){
-//     context.fillStyle="white";
-//     context.fillRect(0,0,canvas.width,canvas.height);
-// }
-
-// //生成随机点
-// function randomPoints(PointsNum){
-//     var generated=[];
-//     for (let i=0;i<PointsNum;i++){
-//         var x=Math.floor(Math.random()*canvas.width);
-//         var y=Math.floor(Math.random()*canvas.height);
-//         var point={x:x-originX,y:y-originY};
-//         generated.push(point);
-//     }
-//     return generated;
-// }
-
-// //画点
-// function drawPoints(dp){
-//     for(let i=0;i<dp.length;i++){
-//         context.fillStyle="white";
-//         context.fillRect(dp[i].x+originX,dp[i].y+originY,1,1);
-//     }
-// }
-
-// //移动点
-// function movePoints(mp){
-//     for(let i=0;i<mp.length;i++){
-//         mp[i].x=mp[i].x+mp[i].x*ScaleX;
-//         mp[i].y=mp[i].y+mp[i].y*ScaleY;
-//         if(mp[i].x<-(canvas.width/2)||mp[i].x>(canvas.width/2)||mp[i].y<-(canvas.height/2)||mp[i].y>(canvas.height/2)){mp.splice(i,1);}
-//     }
-// }
-// }
-// // //过渡动画
-// // //返回
-// // //图片效果
+}}
